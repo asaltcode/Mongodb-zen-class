bcf82fdbcf6a4f828d9db34e6174c4cd125cbd65
asaltcode <elangovanr558@gmail.com>
2024-01-22 09:28:43 +0530

    updated

---
+++ script.js
@@ -0,0 +1,482 @@
+let attendance = [
+
+  {user_id: 1, date: ISODate("2020-10-13"), status: true },
+  {user_id: 1, date: ISODate("2020-10-14"), status: true },
+  {user_id: 1, date: ISODate("2020-10-15"), status: true },
+  {user_id: 1, date: ISODate("2020-10-16"), status: false },
+  {user_id: 1, date: ISODate("2020-10-17"), status: true },
+
+  {user_id: 1, date: ISODate("2020-10-20"), status: true },
+  {user_id: 1, date: ISODate("2020-10-21"), status: true },
+  {user_id: 1, date: ISODate("2020-10-22"), status: true },
+  {user_id: 1, date: ISODate("2020-10-23"), status: true },
+  {user_id: 1, date: ISODate("2020-10-24"), status: true },
+
+  {user_id: 1, date: ISODate("2020-10-27"), status: false },
+  {user_id: 1, date: ISODate("2020-10-28"), status: true },
+  {user_id: 1, date: ISODate("2020-10-29"), status: true },
+  {user_id: 1, date: ISODate("2020-10-30"), status: true },
+  {user_id: 1, date: ISODate("2020-10-31"), status: true },
+  
+
+
+
+  {user_id: 2, date: ISODate("2020-10-13"), status: true },
+  {user_id: 2, date: ISODate("2020-10-14"), status: true },
+  {user_id: 2, date: ISODate("2020-10-15"), status: true },
+  {user_id: 2, date: ISODate("2020-10-16"), status: true },
+  {user_id: 2, date: ISODate("2020-10-17"), status: true },
+  
+
+  {user_id: 2, date: ISODate("2020-10-21"), status: true },
+  {user_id: 2, date: ISODate("2020-10-22"), status: true },
+  {user_id: 2, date: ISODate("2020-10-23"), status: true },
+  {user_id: 2, date: ISODate("2020-10-24"), status: true },
+
+
+  {user_id: 2, date: ISODate("2020-10-27"), status: true },
+  {user_id: 2, date: ISODate("2020-10-28"), status: true },
+  {user_id: 2, date: ISODate("2020-10-29"), status: true },
+  {user_id: 2, date: ISODate("2020-10-30"), status: true },
+  {user_id: 2, date: ISODate("2020-10-31"), status: true },
+
+
+
+  {user_id: 3, date: ISODate("2020-10-13"), status: true },
+  {user_id: 3, date: ISODate("2020-10-14"), status: true },
+  {user_id: 3, date: ISODate("2020-10-15"), status: true },
+  {user_id: 3, date: ISODate("2020-10-16"), status: true },
+  {user_id: 3, date: ISODate("2020-10-17"), status: true },
+
+
+  {user_id: 3, date: ISODate("2020-10-20"), status: true },
+  {user_id: 3, date: ISODate("2020-10-21"), status: true },
+  {user_id: 3, date: ISODate("2020-10-22"), status: true },
+  {user_id: 3, date: ISODate("2020-10-23"), status: true },
+  {user_id: 3, date: ISODate("2020-10-24"), status: true },
+
+
+  {user_id: 3, date: ISODate("2020-10-27"), status: true },
+  {user_id: 3, date: ISODate("2020-10-28"), status: true },
+  {user_id: 3, date: ISODate("2020-10-29"), status: true },
+  {user_id: 3, date: ISODate("2020-10-30"), status: true },
+  {user_id: 3, date: ISODate("2020-10-31"), status: true },
+  {user_id: 3, date: ISODate("2020-11-01"), status: true },
+
+
+  {user_id: 4, date: ISODate("2020-10-13"), status: true },
+  {user_id: 4, date: ISODate("2020-10-14"), status: true },
+  {user_id: 4, date: ISODate("2020-10-15"), status: false },
+  {user_id: 4, date: ISODate("2020-10-16"), status: true },
+  {user_id: 4, date: ISODate("2020-10-17"), status: true },
+  {user_id: 4, date: ISODate("2020-10-18"), status: false },
+
+
+  {user_id: 4, date: ISODate("2020-10-21"), status: true },
+  {user_id: 4, date: ISODate("2020-10-22"), status: true },
+  {user_id: 4, date: ISODate("2020-10-23"), status: false },
+  {user_id: 4, date: ISODate("2020-10-24"), status: true },
+
+  
+  {user_id: 4, date: ISODate("2020-10-27"), status: true },
+  {user_id: 4, date: ISODate("2020-10-28"), status: false },
+  {user_id: 4, date: ISODate("2020-10-29"), status: true },
+  {user_id: 4, date: ISODate("2020-10-30"), status: true },
+  {user_id: 4, date: ISODate("2020-10-31"), status: true },
+  {user_id: 4, date: ISODate("2020-11-01"), status: true },
+
+
+  {user_id: 5, date: ISODate("2020-10-13"), status: true },
+  {user_id: 5, date: ISODate("2020-10-14"), status: true },
+  {user_id: 5, date: ISODate("2020-10-15"), status: true },
+  {user_id: 5, date: ISODate("2020-10-16"), status: false },
+  {user_id: 5, date: ISODate("2020-10-17"), status: true },
+  {user_id: 5, date: ISODate("2020-10-18"), status: true },
+
+
+  {user_id: 5, date: ISODate("2020-10-21"), status: true },
+  {user_id: 5, date: ISODate("2020-10-22"), status: true },
+  {user_id: 5, date: ISODate("2020-10-23"), status: true },
+  {user_id: 5, date: ISODate("2020-10-24"), status: true },
+
+  
+  {user_id: 5, date: ISODate("2020-10-27"), status: true },
+  {user_id: 5, date: ISODate("2020-10-28"), status: true },
+  {user_id: 5, date: ISODate("2020-10-29"), status: true },
+  {user_id: 5, date: ISODate("2020-10-30"), status: true },
+  {user_id: 5, date: ISODate("2020-10-31"), status: false },
+  {user_id: 5, date: ISODate("2020-11-01"), status: true },
+
+]
+
+let tasks = [    
+    {
+    user_id: 1,
+    task_date: ISODate('2020-10-16'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe294'),
+    topic : 'JavaScript - Day -2: Datatypes',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: new Date('2020-10-16'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 1,
+    task_date: ISODate('2020-10-22'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29a'),
+    topic: 'HTML- Day -1: HTML',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-23'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 1,
+    task_date: ISODate('2020-10-27'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29f'),
+    topic: 'DOM- Day -1: DOM',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-28'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+
+    {
+    user_id: 2,
+    task_date: ISODate('2020-10-16'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe294'),
+    topic : 'JavaScript - Day -2: Datatypes',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: '',
+    front_end_Deployed_URL:'',
+    submit_date: ISODate(''),
+    task_mark: '',
+    comments: '',
+    Submission_status: false
+    },
+    {
+    user_id: 2,
+    task_date: ISODate('2020-10-22'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29a'),
+    topic: 'HTML- Day -1: HTML',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-23'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 2,
+    task_date: ISODate('2020-10-27'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29f'),
+    topic: 'DOM- Day -1: DOM',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-28'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+
+    {
+    user_id: 3,
+    task_date: ISODate('2020-10-16'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe294'),
+    topic : 'JavaScript - Day -2: Datatypes',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-16'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 3,
+    task_date: ISODate('2020-10-22'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29a'),
+    topic: 'HTML- Day -1: HTML',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-23'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 3,
+    task_date: ISODate('2020-10-27'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29f'),
+    topic: 'DOM- Day -1: DOM',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-28'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+
+    {
+    user_id: 4,
+    task_date: ISODate('2020-10-16'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe294'),
+    topic : 'JavaScript - Day -2: Datatypes',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-16'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 4,
+    task_date: ISODate('2020-10-22'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29a'),
+    topic: 'HTML- Day -1: HTML',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-23'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 4,
+    task_date: ISODate('2020-10-27'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29f'),
+    topic: 'DOM- Day -1: DOM',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-28'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+
+    {
+    user_id: 5,
+    task_date: ISODate('2020-10-16'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe294'),
+    topic : 'JavaScript - Day -2: Datatypes',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-16'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+    {
+    user_id: 5,
+    task_date: ISODate('2020-10-22'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29a'),
+    topic: 'HTML- Day -1: HTML',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: '',
+    front_end_Deployed_URL:'',
+    submit_date: ISODate(''),
+    task_mark: '',
+    comments: '',
+    Submission_status: false
+    },
+    {
+    user_id: 5,
+    task_date: ISODate('2020-10-27'),
+    topic_id: ObjectId('65ad0b24e2669c68a3ebe29f'),
+    topic: 'DOM- Day -1: DOM',
+    task_document_link: 'https://docs.google.com/document/',
+    front_end_Source_code: 'https://github.com/',
+    front_end_Deployed_URL:'https://example.vercel.app/',
+    submit_date: ISODate('2020-10-28'),
+    task_mark: '',
+    comments: '',
+    Submission_status: true
+    },
+]
+
+let topics = [
+    {
+        topic: 'JavaScript - Day -1: Introduction to Browser & web',
+        class_date : ISODate("2020-10-15")
+        
+    },
+    {
+        topic: 'JavaScript - Day -2: Datatypes',
+        class_date : ISODate("2020-10-16")
+        
+    },
+    {
+        topic: 'JavaScript - Day -3: JS array & objects',
+        class_date : ISODate("2020-10-17")
+        
+    },
+    {
+        topic: 'JavaScript - Day -6: OOP in JS',
+        class_date : ISODate("2020-10-20")
+        
+    },
+    {
+        topic: 'JavaScript - Day -7: Array methods',
+        class_date : ISODate("2020-10-21")
+        
+    },
+    {
+        topic: 'HTML- Day -1: HTML',
+        class_date : ISODate("2020-10-22")
+        
+    },
+    {
+        topic: 'HTML & CSS- Day -2: HTML & CSS',
+        class_date : ISODate("2020-10-23")
+        
+    },
+    {
+        topic: 'HTML & CSS- Day -3: HTML & CSS',
+        class_date : ISODate("2020-10-24")
+        
+    },
+    {
+        topic: 'DOM- Day -1: DOM',
+        class_date : ISODate("2020-10-27")
+        
+    },
+    {
+        topic: 'DOM- Day -2: Document vs Window',
+        class_date : ISODate("2020-10-28")
+        
+    },
+    {
+        topic: 'DOM- Day -3: Recap of Topics',
+        class_date : ISODate("2020-10-29")
+        
+    },
+]
+
+let users =[    
+{
+_id : ObjectId('65ac6d66e2669c68a3ebe194'),
+name : "Elangovan",
+mobile : "1234567890",
+batch : "B53",
+email : "elangovan@gmail.com"
+},
+{
+_id : ObjectId('65ac6d66e2669c68a3ebe195'),
+name : "Bill Gates",
+mobile : "1231117890",
+batch : "B53",
+email : "billgates@gmail.com"
+},
+{
+_id : ObjectId('65ac6d66e2669c68a3ebe196'),
+name : "Donald Knuth",
+mobile : "1200067890",
+batch : "B53",
+email : "donaldknuth@gmail.com"
+},
+{
+_id : ObjectId('65ac6d66e2669c68a3ebe197'),
+name : "Brendan Eich",
+mobile : "1200067890",
+batch : "B53",
+email : "brendaneich@gmail.com"
+},
+{
+_id : ObjectId('65ac6d66e2669c68a3ebe198'),
+name : "Guido van Rossum",
+mobile : "1200067890",
+batch : "B53",
+email : "guidovanrossum@gmail.com"
+},
+]
+
+let codekata = [
+
+    
+    {user_id: 1, solution: true, Total_solved : 10, problem: 'array'},
+    {user_id: 1, solution: true, Total_solved : 20, problem: 'for loop'},
+    {user_id: 1, solution: false, Total_solved : 0, problem: 'forEach'},
+    {user_id: 1, solution: true, Total_solved : 40, problem: 'if contion'},
+    {user_id: 1, solution: true, Total_solved : 30, problem: 'while loop'},
+    {user_id: 1, solution: false, Total_solved : 0, problem: 'map'},
+    {user_id: 1, solution: true, Total_solved : 30, problem: 'filter'},
+    {user_id: 1, solution: false, Total_solved : 0, problem: 'reduce'},
+
+    
+    {user_id: 2, solution: true, Total_solved : 10, problem: 'array'},
+    {user_id: 2, solution: true, Total_solved : 20, problem: 'for loop'},
+    {user_id: 2, solution: true, Total_solved : 30, problem: 'forEach'},
+    {user_id: 2, solution: true, Total_solved : 40, problem: 'if contion'},
+    {user_id: 2, solution: true, Total_solved : 30, problem: 'while loop'},
+    {user_id: 2, solution: true, Total_solved : 0, problem: 'map'},
+    {user_id: 2, solution: true, Total_solved : 30, problem: 'filter'},
+    {user_id: 2, solution: true, Total_solved : 0, problem: 'reduce'},
+
+    
+    {user_id: 3, solution: false, Total_solved : 0, problem: 'array'},
+    {user_id: 3, solution: false, Total_solved : 0, problem: 'for loop'},
+    {user_id: 3, solution: true, Total_solved : 30, problem: 'forEach'},
+    {user_id: 3, solution: true, Total_solved : 40, problem: 'if contion'},
+    {user_id: 3, solution: true, Total_solved : 30, problem: 'while loop'},
+    {user_id: 3, solution: false, Total_solved : 0, problem: 'map'},
+    {user_id: 3, solution: true, Total_solved : 30, problem: 'filter'},
+    {user_id: 3, solution: false, Total_solved : 0, problem: 'reduce'},
+
+    
+    {user_id: 4, solution: true, Total_solved : 10, problem: 'array'},
+    {user_id: 4, solution: false, Total_solved : 0, problem: 'for loop'},
+    {user_id: 4, solution: true, Total_solved : 30, problem: 'forEach'},
+    {user_id: 4, solution: true, Total_solved : 40, problem: 'if contion'},
+    {user_id: 4, solution: true, Total_solved : 30, problem: 'while loop'},
+    {user_id: 4, solution: true, Total_solved : 20, problem: 'map'},
+    {user_id: 4, solution: true, Total_solved : 30, problem: 'filter'},
+    {user_id: 4, solution: false, Total_solved : 0, problem: 'reduce'},
+
+    
+    {user_id: 5, solution: true, Total_solved : 10, problem: 'array'},
+    {user_id: 5, solution: true, Total_solved : 20, problem: 'for loop'},
+    {user_id: 5, solution: false, Total_solved : 0, problem: 'forEach'},
+    {user_id: 5, solution: true, Total_solved : 40, problem: 'if contion'},
+    {user_id: 5, solution: true, Total_solved : 30, problem: 'while loop'},
+    {user_id: 5, solution: false, Total_solved : 0, problem: 'map'},
+    {user_id: 5, solution: true, Total_solved : 30, problem: 'filter'},
+    {user_id: 5, solution: false, Total_solved : 0, problem: 'reduce'},
+]
+
+let companydrives = [
+    { user_id: 1 , drive_date: ISODate("2020-10-20"), company: "Apple" },
+    { user_id: 2 , drive_date: ISODate("2020-10-22"), company: "Amazon" },
+    { user_id: 3 , drive_date: ISODate("2020-10-25"), company: "TCS" },
+    { user_id: 4 , drive_date: ISODate("2020-10-05"), company: "Zomato" },
+    { user_id: 2 , drive_date: ISODate("2020-10-30"), company: "Flipkart" },
+]
+
+let mentors = [
+    {mentorname: "Rupan", mentor_email: "rupan@gmail.com", mentee_count: 20 },
+    {mentorname: "Nagaraj", mentor_email: "nagaraj@gmail.com", mentee_count: 18 },
+    {mentorname: "Isak Andic", mentor_email: "isakandic@gmail.com", mentee_count: 30 },
+    {mentorname: "Nahman Andic", mentor_email: "nahmanandic@gmail.com", mentee_count: 15 },
+    {mentorname: "Ram Kumar", mentor_email: "ramkumar@gmail.com", mentee_count: 13 },
+]
+
+
+
+
+
